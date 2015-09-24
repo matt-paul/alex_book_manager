e59b3b7eb12d0e7ac750437e252d7bc78e7c4ce1
Alexander Person <alexander@Leonards-MacBook-Pro.local>
2015-09-24 18:12:10 +0100

    Displays flash notice when password and confirmation password do not match

--- app/app.rb
+++ app/app.rb
@@ -1,11 +1,13 @@
 require 'sinatra/base'
 require_relative 'data_mapper_setup.rb'
+require 'sinatra/flash'
 # require 'byebug'
 
 class BookmarkManager < Sinatra::Base
 
   enable :sessions
   set :session_secret, 'super secret'
+  register Sinatra::Flash
 
   helpers do
     def current_user
@@ -52,8 +54,13 @@ class BookmarkManager < Sinatra::Base
     user = User.create(email: params[:email],
                 password: params[:password],
                 password_confirmation: params[:password_confirmation])
-    session[:user_id] = user.id
-    redirect to('/links')
+    if user.save
+      session[:user_id] = user.id
+      redirect to('/links')
+    else
+      flash.now[:notice] = "Password and confirmation password do not match"
+      erb :'users/new'
+    end
   end
 
 
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -6,8 +6,10 @@ feature 'User sign up' do
     expect(User.first.email).to eq('alice@example.com')
   end
 
-  scenario 'requires a matching confirmation password' do
+  scenario 'with a password that does not match' do
     expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    expect(current_path).to eq('/users')
+    expect(page).to have_content 'Password and confirmation password do not match'
   end
 
   def sign_up(email: 'alice@example.com',
